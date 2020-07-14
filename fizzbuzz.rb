8ffc839bc7fa9bc0d05d18b841bdb0b09fa0faf0
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-14 15:08:45 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+    "FizzBuzz"
+  elsif number % 5 == 0
+    puts "Buzz"
+    "Buzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+    "Fizz"
+  else
+    puts "#{number}"
+  end
+end
