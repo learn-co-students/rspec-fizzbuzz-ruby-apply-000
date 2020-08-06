3108f32f096c5a056f2930fc552b0022e1948f54
ADAM YE <adamye48@gmail.com>
2020-08-06 21:05:01 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,20 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0
+   puts "Fizz"
+  end
+  
+  if int % 5 == 0 
+   puts "Buzz"
+  end
+  
+   if int % 3 & 5 == 0 
+     puts "Fizzbuzz"
+   end
+   
+   if int % 3 & 5 != 0 
+    nil
+  end
+end
