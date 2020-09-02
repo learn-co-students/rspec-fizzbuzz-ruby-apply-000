0b2ea41955ef7f180878b9c80a0aab2b0e9abbc5
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-02 20:27:15 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(user_input)
+    if user_input % 3 == 0 && user_input % 5 == 0
+    return "FizzBuzz"
+
+    elsif user_input % 3 == 0
+    return "Fizz"
+
+    elsif user_input % 5 == 0
+    return "Buzz"
+
+    elsif user_input % 3 != 0 && user_input % 5 != 0
+    return nil
+  end
+end
