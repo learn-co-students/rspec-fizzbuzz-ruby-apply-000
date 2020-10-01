62ebb37d7d587aeef48dee0f3f4856bae44a1815
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 02:38:46 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(number)
+  if number == 3
+    return "Fizz"
+  elsif
+    number == 5
+    return "Buzz"
+  elsif
+    number == 15
+    return "FizzBuzz"
+  else
+    number == 4
+    return nil
+  end
+end
